6c1548f37a15079f29b814ca73a3f68860a3ccec
Frank <cfwei@Frank-Ubuntu.(none)>
2012-07-25 00:30:18 +0800

    initial version

---
+++ src/com/example/takenumbersystemstore/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.takenumbersystemstore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+	public String ServerURL="http://192.168.0.100/";
+	private String Store_ID,Store_passwd; 
+	SharedPreferences account_settings;
+	Button LoginButtonA;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        account_settings = getSharedPreferences ("ACCOUNT", 0);
+        Store_ID=account_settings.getString("Store_ID","");
+        Store_passwd=account_settings.getString("Store_passwd","");
+    	final EditText ID=(EditText)findViewById(R.id.IDeditText);
+    	final EditText Passwd=(EditText)findViewById(R.id.PasswordeditText);
+        
+    	ID.setText(Store_ID);
+    	Passwd.setText(Store_passwd);
+        
+        LoginButtonA=(Button)findViewById(R.id.LoginButton);
+        LoginButtonA.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+			
+				Store_ID=ID.getText().toString();
+				Store_passwd=Passwd.getText().toString();
+				
+				//儲存preference
+				
+				SharedPreferences.Editor PE = account_settings.edit();
+				PE.putString("Store_ID", Store_ID);
+				PE.putString("Store_passwd", Store_passwd);
+				PE.commit();
+				
+			}
+		});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    
+    public String connect_to_server(String program,ArrayList<NameValuePair> nameValuePairs) throws ClientProtocolException, IOException
+    {	
+    	//建立一個httpclient
+    	HttpClient httpclient = new DefaultHttpClient();
+    	//設定httppost的網址
+    	HttpPost httppost = new HttpPost(ServerURL+program);
+    	
+    	//加入參數
+    	if(nameValuePairs!=null)
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+    	
+		//發出httppost要求並接收回傳轉成字串
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity entity = response.getEntity();
+		InputStream is = entity.getContent();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"),8);
+		String line = null;
+		StringBuilder sb = new StringBuilder();
+		while ((line = reader.readLine()) != null) 
+		{
+			sb.append(line);
+		}
+		is.close();
+		String result = sb.toString();
+		
+		return result;
+    }
+    
+    public ArrayList<HashMap<String,String>> json_deconde(String jsonString,String[] key) throws JSONException
+    {	
+    	ArrayList<HashMap<String,String>> item = new ArrayList<HashMap<String,String>>();
+    	JSONArray jArray = new JSONArray(jsonString);
+    	for(int i = 0;i<jArray.length();i++)
+		{	
+    		 HashMap<String,String> temp = new HashMap<String,String>();
+	     	 JSONObject json_data = jArray.getJSONObject(i); 
+	     	 for(int j=0;j<key.length;j++)
+	     	 	temp.put(key[j], json_data.getString(key[j]));
+	     	 item.add(temp);
+	     	 //Toast.makeText(this, json_data.getString(key[2]), Toast.LENGTH_SHORT).show();
+		}
+		
+	
+		return item;
+    	
+    }
+
+    
+}
